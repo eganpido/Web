90b736a0b1e39b1cd1864b5e5fbcb9314a854a0b
eganpido <eganpido90@gmail.com>
2017-07-21 16:13:01 +0800

    commit 1

---
+++ WebApplication1/ApiControllers/ArticleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.ApiControllers
+{
+    public class ArticleController : ApiController
+    {
+        private Data.sample_dbDataContext db = new Data.sample_dbDataContext();
+
+        [HttpGet]
+        [Authorize]
+        [Route("api/list/article")]
+        public List<Entities.Article> listArticle()
+        {
+            var articles = from d in db.MstArticles.OrderBy(d => d.Article)
+                           select new Entities.Article
+                           {
+                               Id = d.Id,
+                               ArticleCode = d.ArticleCode,
+                               ManualArticleCode = d.ManualArticleCode,
+                               ArticleName = d.Article
+                           };
+
+            return articles.ToList();
+        }
+
+    }
+}
---
+++ WebApplication1/Entities/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Entities
+{
+    public class Article 
+    {
+	    public int Id {get; set;}
+        public String ArticleCode { get; set; }
+        public String ManualArticleCode { get; set; }
+        public String ArticleName { get; set; }
+	}
+}
